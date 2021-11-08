e3c8c99a2aa6d09a435ae1a66065909f50c3002e
Doris Morales <moralesdoris880@gmail.com>
2021-11-08 18:43:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name,email){
+    let user = {
+        'name' : name ,
+        'email' : email
+    }
+    
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+           'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+        document.body.innerHTML = error.message
+      })
+}
+
+submitData();
+
+
+    
